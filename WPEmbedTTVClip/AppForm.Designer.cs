a29956e36e73ab134b497aff76b07124f3e03607
koishibuh <elysiagriffin@gmail.com>
2023-02-07 20:22:06 +0000

    Initial commit to Github! Huzzah

---
+++ WPEmbedTTVClip/AppForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace URLConvert
+{
+    partial class AppForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppForm));
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.WordpressCodeLabel = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.DomainTextBox = new System.Windows.Forms.TextBox();
+            this.VideoHeightTextBox = new System.Windows.Forms.TextBox();
+            this.VideoWidthTextBox = new System.Windows.Forms.TextBox();
+            this.DomainLabel = new System.Windows.Forms.Label();
+            this.HeightLabel = new System.Windows.Forms.Label();
+            this.WidthLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ConvertButton.Location = new System.Drawing.Point(11, 59);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(326, 41);
+            this.ConvertButton.TabIndex = 3;
+            this.ConvertButton.Text = "CONVERT";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // WordpressCodeLabel
+            // 
+            this.WordpressCodeLabel.Location = new System.Drawing.Point(12, 126);
+            this.WordpressCodeLabel.Name = "WordpressCodeLabel";
+            this.WordpressCodeLabel.Size = new System.Drawing.Size(325, 109);
+            this.WordpressCodeLabel.TabIndex = 4;
+            this.WordpressCodeLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.StatusLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.StatusLabel.Location = new System.Drawing.Point(11, 103);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(326, 23);
+            this.StatusLabel.TabIndex = 5;
+            this.StatusLabel.Text = "Ready To Convert Clipboard Link";
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // DomainTextBox
+            // 
+            this.DomainTextBox.Location = new System.Drawing.Point(35, 30);
+            this.DomainTextBox.Name = "DomainTextBox";
+            this.DomainTextBox.Size = new System.Drawing.Size(131, 23);
+            this.DomainTextBox.TabIndex = 6;
+            // 
+            // VideoHeightTextBox
+            // 
+            this.VideoHeightTextBox.Location = new System.Drawing.Point(263, 30);
+            this.VideoHeightTextBox.Name = "VideoHeightTextBox";
+            this.VideoHeightTextBox.Size = new System.Drawing.Size(54, 23);
+            this.VideoHeightTextBox.TabIndex = 7;
+            this.VideoHeightTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.VideoHeightTextBox_KeyPress);
+            // 
+            // VideoWidthTextBox
+            // 
+            this.VideoWidthTextBox.Location = new System.Drawing.Point(200, 30);
+            this.VideoWidthTextBox.Name = "VideoWidthTextBox";
+            this.VideoWidthTextBox.Size = new System.Drawing.Size(54, 23);
+            this.VideoWidthTextBox.TabIndex = 8;
+            this.VideoWidthTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.VideoWidthTextBox_KeyPress);
+            // 
+            // DomainLabel
+            // 
+            this.DomainLabel.AutoSize = true;
+            this.DomainLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DomainLabel.Location = new System.Drawing.Point(73, 8);
+            this.DomainLabel.Name = "DomainLabel";
+            this.DomainLabel.Size = new System.Drawing.Size(57, 19);
+            this.DomainLabel.TabIndex = 9;
+            this.DomainLabel.Text = "Domain";
+            // 
+            // HeightLabel
+            // 
+            this.HeightLabel.AutoSize = true;
+            this.HeightLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.HeightLabel.Location = new System.Drawing.Point(265, 9);
+            this.HeightLabel.Name = "HeightLabel";
+            this.HeightLabel.Size = new System.Drawing.Size(50, 19);
+            this.HeightLabel.TabIndex = 10;
+            this.HeightLabel.Text = "Height";
+            // 
+            // WidthLabel
+            // 
+            this.WidthLabel.AutoSize = true;
+            this.WidthLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.WidthLabel.Location = new System.Drawing.Point(204, 9);
+            this.WidthLabel.Name = "WidthLabel";
+            this.WidthLabel.Size = new System.Drawing.Size(46, 19);
+            this.WidthLabel.TabIndex = 11;
+            this.WidthLabel.Text = "Width";
+            // 
+            // AppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 237);
+            this.Controls.Add(this.WidthLabel);
+            this.Controls.Add(this.HeightLabel);
+            this.Controls.Add(this.DomainLabel);
+            this.Controls.Add(this.VideoWidthTextBox);
+            this.Controls.Add(this.VideoHeightTextBox);
+            this.Controls.Add(this.DomainTextBox);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.WordpressCodeLabel);
+            this.Controls.Add(this.ConvertButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "AppForm";
+            this.Text = "WP Embed TTV Clip";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button ConvertButton;
+        private Label WordpressCodeLabel;
+        private Label StatusLabel;
+        private TextBox DomainTextBox;
+        private TextBox VideoHeightTextBox;
+        private TextBox VideoWidthTextBox;
+        private Label DomainLabel;
+        private Label HeightLabel;
+        private Label WidthLabel;
+    }
+}
---
+++ WPEmbedTTVClip/AppForm.cs
@@ -0,0 +1,211 @@
+using System;
+using System.Data;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Policy;
+using WPEmbedClipCode;
+
+namespace URLConvert
+{
+    public partial class AppForm : Form
+    {
+        string DomainURL;
+        int VideoHeight;
+        int VideoWidth;
+        string wordpressCode = "";
+        string ErrorMessageWPCodeDuplicate = "Wordpress Code already copied to clipboard";
+        string ErrorMessageNotTwitchURL = "Not a proper Twitch clip URL";
+        string ErrorMessageNotValidDomain = "Not a valid Domain";
+
+        public AppForm()
+        {
+            InitializeComponent();
+            InitializeAppForm();
+        }
+
+        public void InitializeAppForm()
+        {
+            DomainTextBox.Text = Settings.Default.Domain;
+            VideoHeightTextBox.Text = Settings.Default.VideoHeight;
+            VideoWidthTextBox.Text = Settings.Default.VideoWidth;
+
+            FormClosing += AppFormClosingEventHandler;
+        }
+
+        public void AppFormClosingEventHandler(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.Domain = DomainTextBox.Text ;
+            Settings.Default.VideoHeight = VideoHeightTextBox.Text;
+            Settings.Default.VideoWidth = VideoWidthTextBox.Text;
+            Settings.Default.Save();
+
+            //TODO: If domain is not valid, do not save? Close? Something
+        }
+
+        public void SaveFieldValuesToSettings()
+        {
+            Settings.Default.Domain = DomainTextBox.Text;
+            Settings.Default.VideoHeight = VideoHeightTextBox.Text;
+            Settings.Default.VideoWidth = VideoWidthTextBox.Text;
+        }
+
+        public async void ConvertButton_Click(object sender, EventArgs e)
+        {
+            //TODO: CheckIfDomainHasChanged
+            bool isValid = await CheckIfDomainIsValid();
+            if (isValid == true)
+            {
+                WordpressCodeLabel.Text = "";
+                ConvertButton.Enabled = false;
+                string url = Clipboard.GetText();
+
+                if (CheckIfClipboardIsWPCode(url) == true)
+                {
+                    await DisplayErrorMessage(ErrorMessageWPCodeDuplicate, true);
+                    return;
+                };
+
+                AttemptToConvertTwitchURL(url);
+            }
+            return;     
+        }
+
+        public bool CheckIfClipboardIsWPCode(string clipboard)
+        {
+            //return !clipboard.StartsWith("<br>");
+
+            if (string.IsNullOrEmpty(wordpressCode))
+            {
+                return false;   
+            }
+            else if ( clipboard == wordpressCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public async Task<bool> CheckIfDomainIsValid()
+        {
+            string domainURL = DomainTextBox.Text;
+
+            if (domainURL.StartsWith("www."))
+            {
+                if (domainURL.Contains('/'))
+                {
+                    string rootDomainURL = domainURL.Substring(0, domainURL.LastIndexOf("/"));
+
+                    DomainTextBox.Text = rootDomainURL;
+                }
+            }
+            else if (domainURL.StartsWith("http"))
+            {
+                string noProtcolDomainURL = domainURL.Substring(domainURL.IndexOf("w"));
+
+                if (noProtcolDomainURL.Contains("/"))
+                {
+                    string rootDomainURL = noProtcolDomainURL.Substring(0, noProtcolDomainURL.LastIndexOf("/"));
+                    DomainTextBox.Text = rootDomainURL;
+                }
+
+                DomainTextBox.Text = noProtcolDomainURL;
+            }
+            else if (!domainURL.StartsWith("www.")) //TODO: Check if URL is valid 
+            {
+                string wwwAddedDomainURL = "www." + domainURL;
+                if (Uri.CheckHostName(wwwAddedDomainURL) == UriHostNameType.Unknown)
+                {
+                    await DisplayErrorMessage(ErrorMessageNotValidDomain, false);
+                    return false;
+                }               
+
+                DomainTextBox.Text = wwwAddedDomainURL;
+            }
+           
+            SaveFieldValuesToSettings();
+            return true;
+        }
+
+        public async Task AttemptToConvertTwitchURL(string url)
+        {
+            string twitchURL = url;
+            string trimmedTwitchURL;            
+
+            if (twitchURL.Contains("https://clips.twitch.tv/") || 
+                (twitchURL.Contains("https://www.twitch.tv/") && twitchURL.Contains("/clip/")))
+            {
+                trimmedTwitchURL = twitchURL.Substring(twitchURL.LastIndexOf("/") + 1);
+                if (trimmedTwitchURL.Contains('?'))
+                {
+                   trimmedTwitchURL = trimmedTwitchURL.Substring(0, trimmedTwitchURL.IndexOf("?"));
+                }
+            }
+            else
+            {
+                await DisplayErrorMessage(ErrorMessageNotTwitchURL, true);
+                return;
+            }
+
+            wordpressCode = BuildWordpressCode(trimmedTwitchURL);
+
+            DisplayWordpressCode(wordpressCode);
+        }
+
+        public string BuildWordpressCode(string url)
+        {
+            return 
+                $"<pre class=\"wp-block-code\"><div id=\"iframe-wrapper\" align=\"center\">" +
+                $"<iframe src=\"https://clips.twitch.tv/embed?clip=" +
+                $"{url}&amp;parent={DomainTextBox.Text}\" " +
+                $"allowfullscreen=\"true\" scrolling=\"no\" width=\"{VideoWidthTextBox.Text}\" height=\"{VideoHeightTextBox.Text}\" " +
+                $"frameborder=\"0\"></iframe></div></pre>";
+        }
+
+        public void DisplayWordpressCode(string code)
+        {
+            StatusLabel.Text = "Conversion Completed";
+            StatusLabel.ForeColor = Color.Green;
+            WordpressCodeLabel.Text = code;
+            Clipboard.SetText(code);
+            ConvertButton.Enabled = true;
+        }
+
+        public async Task DisplayErrorMessage(string errorMessage, bool showClipboard)
+        {
+            StatusLabel.ForeColor = Color.Red;
+            StatusLabel.Text = errorMessage;
+            if ( showClipboard == true ) 
+            {
+                WordpressCodeLabel.Text = Clipboard.GetText();
+            }
+            await Task.Delay(2500);
+            WordpressCodeLabel.Text = "";
+            StatusLabel.Text = "Ready to Convert";
+            StatusLabel.ForeColor = Color.Black;
+            ConvertButton.Enabled = true;
+        }
+
+        public async void VideoHeightTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            { 
+                e.Handled = true;
+                await DisplayErrorMessage("Numeric values only", false);
+            }
+            if (e.KeyChar == (char)8) e.Handled = false; // Allows backspacing
+        }
+
+        public async void VideoWidthTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                await DisplayErrorMessage("Numeric values only", false);
+            }
+            if (e.KeyChar == (char)8) e.Handled = false; // Allows backspacing
+        }
+    }
+}
